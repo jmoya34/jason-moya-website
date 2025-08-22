b29c1f98ad536290ca0c33a43dc4d0e78adf1ce3
jmoya34 <jasonmoya34@gmail.com>
2025-08-21 18:45:33 -0700

    mediaQuery checkpoint, fixed client issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,18 @@
-"use client"
-
-import { ReactNode } from "react";
 import AppBar from "../../components/appbar";
-import SocialButton from "../../components/socials";
-import { useMediaQuery } from "@uidotdev/usehooks";
-
+import { HomeContent } from "../../components/home_components/HomeBlocks";
+import { ClientOnly } from "../../components/mediaQ";
 
 export default function Home() {
-  const isMobile = useMediaQuery("only screen and (max-width : 640px)");
 
   return (
-    <div className=" min-h-screen pb-20">
-      <main >
-        <AppBar/>
+    <div className="min-h-screen pb-20">
+      <main>
+        <AppBar />
       </main>
 
-      <div className="sm:grid sm:grid-cols-2 gap-2 justify-items-center">
-        {isMobile ? 
-            <>
-              <SocialButton/>
-              <AboutBlock/>
-              <MissionBlock/>
-            </>
-          : <>
-              <AboutBlock/>
-              <div className="">
-                <SocialButton/>
-                <MissionBlock/>
-              </div>
-            </> 
-          
-        }
-        
-        
-      </div>
-
-    </div>
-  );
-}
-
-export function AboutBlock(){
-  return (
-    <div className="grid grid-cols-2 pl-4 pr-4  sm:pt-4 gap-5 justify-items-center items-center">
-      <img className="w-50 h-auto" src="/assets/headshot.png" alt="Headshot"/>
-      <CardBlock>
-        <h1 className="pb-4 sm:text-[24px] [@media(min-width:640px)_and_(max-width:934px)]:text-[16px] text-[14px]">Electrical and Computer Engineer</h1>
-        <h1 className="sm:text-[24px] [@media(min-width:640px)_and_(max-width:934px)]:text-[16px] text-[14px]">Pursing masters in RF, Analog, and Mixed Signal Design.</h1>
-      </CardBlock>
-    </div>
-  );
-}
-
-
-export function MissionBlock(){
-  return (
-    <div className="pl-4 pr-4 pb-4 pt-4 sm:pt-4 gap-5 justify-items-center items-center">
-      <CardBlock>
-        <h1 className="pb-4 sm:text-[24px] [@media(min-width:640px)_and_(max-width:934px)]:text-[16px] text-[14px]">My mission in life is to become a respected engineer in the industry, with the experience and wisdom to teach future generations of Electrical and Computer Engineers as a professor at a respected university.</h1>
-      </CardBlock>
-    </div>
-  );
-}
-
-
-
-type cardProps = {
-  className?: string;
-  children: ReactNode;
-};
-
-export function CardBlock({className, children}: cardProps){
-  return (
-    <div className={`card_color p-5 w-fit h-fit rounded-lg shadow-lg shadow-emerald-700/50 ${className ?? ""}`}>
-      {children}
+      <ClientOnly>
+        <HomeContent />
+      </ClientOnly>
     </div>
   );
 }
